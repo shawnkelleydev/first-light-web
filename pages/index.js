702bb2665ef3665c09c924970bf0f07d5f138f79
Shawn Kelley <skelley@us.ci.org>
2022-09-28 14:39:54 -0600

    add ISR to home

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,22 @@
-import { useEffect, useState } from 'react'
-
 import PageWrapper from 'components/PageWrapper'
 import Verse from 'components/Verse'
 
 import { getEarthPicData } from 'services/nasa'
 
-export default function Home() {
-  const [pic, setPic] = useState(null)
-
-  useEffect(() => {
-    const getPic = async () => {
-      const pic = await getEarthPicData()
-      setPic(pic)
-    }
-    getPic()
-  }, [])
-
+export default function Home({ earthPic }) {
   return (
-    <PageWrapper backgroundImage={pic}>
+    <PageWrapper backgroundImage={earthPic}>
       <Verse />
     </PageWrapper>
   )
+}
+
+export const getStaticProps = async () => {
+  const earthPic = await getEarthPicData()
+  return {
+    props: {
+      earthPic,
+    },
+    revalidate: 86400,
+  }
 }
